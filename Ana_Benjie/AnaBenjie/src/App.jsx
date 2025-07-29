87254f310b133e0289921802e72725348141af62
XxAsSouza <beatriz_ana58@hotmail.com>
2025-07-29 18:30:43 -0300

    .

--- Ana_Benjie/AnaBenjie/src/App.jsx
+++ Ana_Benjie/AnaBenjie/src/App.jsx
@@ -7,7 +7,7 @@ import Dicas from "./pages/Dicas"; // importa sua nova página
 import ComoChegar from "./pages/ComoChegar";
 import Citacao from "./components/citacao/Citacao";
 import Countdown from "./components/countdown/Countdown"
-import Gallery from "./components/gallery/gallery";
+import Gallery from "./components/gallery/Gallery";
 import Confirmation from "./components/confirmation/Confirmation";
 import ListRegalos from "./components/listaRegalos/ListRegalos";
 import Footer from "./components/footer/Footer";
