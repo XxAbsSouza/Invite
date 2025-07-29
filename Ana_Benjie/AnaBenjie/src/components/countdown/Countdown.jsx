f77a7054478480235c047964e011329b796dc3b6
XxAsSouza <beatriz_ana58@hotmail.com>
2025-07-29 03:17:20 -0300

    flip-clok responsivo

--- Ana_Benjie/AnaBenjie/src/components/countdown/Countdown.jsx
+++ Ana_Benjie/AnaBenjie/src/components/countdown/Countdown.jsx
@@ -6,8 +6,8 @@ import CountdownTimer from './CountdownTimer';
 const Countdown = () => {
   return (
     <div className="flex flex-col items-center p-12 text-center">
-      <h1 className='mb-8 text-3xl font-bold'>Quedan</h1>
-      <div className='max-w-[100vw]'>
+      <h1 className='my-5 text-3xl font-bold'>Quedan</h1>
+      <div className='mt-6 max-w-[100vw]'>
         <CountdownTimer />
         <p className='my-6'>para que entre nosotros ya no hayan 6300km</p>
       </div>
