e465967eec12a3a84b793fd9b69816778fbc24c4
XxAsSouza <beatriz_ana58@hotmail.com>
2025-08-01 11:27:02 -0300

    layout

--- Ana_Benjie/AnaBenjie/src/App.jsx
+++ Ana_Benjie/AnaBenjie/src/App.jsx
@@ -1,20 +1,37 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Outlet,
+} from "react-router-dom";
 
 import { Hero } from "./components/hero/Hero";
 import InfoGeneral from "./components/Info/InfoGeneral";
-import Dicas from "./pages/Dicas"; // importa sua nova página
+import Dicas from "./pages/Dicas";
 import ComoChegar from "./pages/ComoChegar";
 import Citacao from "./components/citacao/Citacao";
-import Countdown from "./components/countdown/Countdown"
+import Countdown from "./components/countdown/Countdown";
 import Gallery from "./components/gallery/Gallery";
 import Confirmation from "./components/confirmation/Confirmation";
 import ListRegalos from "./components/listaRegalos/ListRegalos";
 import Footer from "./components/footer/Footer";
-import FormConfirm from "./pages/FormConfirm"
+import FormConfirm from "./pages/FormConfirm";
 import SearchGuest from "./components/confirmation/SearchGuest";
 import MessageSorry from "./components/confirmation/MessageSorry";
 import ResponseReport from "./pages/ResponseReport";
+
+// Componente base de layout
+const BaseLayout = ({ showConfirmation = false, showFooter = false }) => (
+  <div className="w-full overflow-hidden text-xl flex flex-col items-center">
+    <Outlet />
+    <div className="w-2/4 my-5 h-px bg-gradient-to-r from-transparent via-gray-400 to-transparent"></div>
+
+    {showConfirmation && <Confirmation />}
+    <ListRegalos />
+    {showFooter && <Footer />}
+  </div>
+);
 
 function App() {
   return (
@@ -36,12 +53,20 @@ function App() {
           }
         />
 
-        <Route path="/dicas" element={<Dicas />} />
-        <Route path="/comoChegar" element={<ComoChegar />} />
-        <Route path="/formsConfirmation" element={<FormConfirm />} />
-        <Route path="/searchGuest" element={<SearchGuest />} />
-        <Route path="/messageSorry" element={<MessageSorry />} />
-        <Route path="/responseReport" element={<ResponseReport />} />
+        <Route element={<BaseLayout />}>
+          <Route path="/messageSorry" element={<MessageSorry />} />
+        </Route>
+
+        <Route element={<BaseLayout showFooter />}>
+          <Route path="/searchGuest" element={<SearchGuest />} />
+          <Route path="/responseReport" element={<ResponseReport />} />
+          <Route path="/formsConfirmation" element={<FormConfirm />} />
+        </Route>
+
+        <Route element={<BaseLayout showConfirmation showFooter />}>
+          <Route path="/dicas" element={<Dicas />} />
+          <Route path="/comoChegar" element={<ComoChegar />} />
+        </Route>
       </Routes>
     </Router>
   );
